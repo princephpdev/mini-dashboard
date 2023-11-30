0873a8fa737ec23fd1617bdc74dfe80a58677d17
princephpdev <princephpdev@gmail.com>
2023-11-30 09:08:30 +0530

    Updated Angular navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
 import { HttpClientModule } from "@angular/common/http";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, NavbarComponent],
 })
 export class AppComponent {
   title = "mini-dashboard";
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { fadeInOut } from "../../utils/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { ButtonComponent } from "../button/button.component";
+import { NavigationEnd, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-navbar',
+  selector: "app-navbar",
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  templateUrl: "./navbar.component.html",
+  styleUrl: "./navbar.component.scss",
+  animations: [fadeInOut],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, ButtonComponent],
 })
 export class NavbarComponent {
+  showNavigation: boolean = false;
 
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showNavigation =
+          event.urlAfterRedirects === "/table" ||
+          event.urlAfterRedirects === "/directive-screen";
+      }
+    });
+  }
 }
