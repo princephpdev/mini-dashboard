e13e8d4b7b535c320132d70f9774708bac7ac090
princephpdev <princephpdev@gmail.com>
2023-11-30 08:48:55 +0530

    Added table with sort and filter

--- src/app/pages/tablescreen/tablescreen.component.ts
+++ src/app/pages/tablescreen/tablescreen.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MatTableDataSource, MatTableModule } from "@angular/material/table";
-import { MatSort } from "@angular/material/sort";
+import { MatSort, MatSortModule, Sort } from "@angular/material/sort";
 import { MatPaginator, MatPaginatorModule } from "@angular/material/paginator";
 import { DataService } from "../../services/data.service";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
-import { ExampleData } from "../../utils/example.interface";
 
 @Component({
   selector: "app-tablescreen",
   standalone: true,
   imports: [
     CommonModule,
-    MatFormFieldModule,
     MatTableModule,
     MatPaginatorModule,
+    MatFormFieldModule,
     MatInputModule,
+    MatSortModule,
   ],
   providers: [DataService],
   templateUrl: "./tablescreen.component.html",
   styleUrl: "./tablescreen.component.scss",
 })
-export class TablescreenComponent implements OnInit {
+export class TablescreenComponent implements AfterViewInit {
   displayedColumns: string[] = [
     "id",
     "type",
@@ -31,28 +31,73 @@ export class TablescreenComponent implements OnInit {
     "relationships",
   ];
   data: any = {};
-  dataSource: MatTableDataSource<ExampleData> = new MatTableDataSource();
+  dataSource: MatTableDataSource<any> = new MatTableDataSource(this.data);
+  columnFilters: { [key: string]: string } = {};
 
   constructor(private dataService: DataService) {}
 
-  @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.dataService.getData().subscribe((result) => {
       this.data = result.data;
-      this.dataSource.data = this.data;
+      this.dataSource = new MatTableDataSource(this.data);
+      this.dataSource.sortingDataAccessor = this.customSortingDataAccessor;
       this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.paginator;
+      this.dataSource.filterPredicate = this.customFilterPredicate;
     });
   }
 
-  applyFilter(event: Event): void {
+  customSortingDataAccessor = (
+    data: any,
+    sortHeaderId: string
+  ): string | number => {
+    const [column, subColumn] = sortHeaderId.split(".");
+    if (subColumn) {
+      return data[column][subColumn];
+    }
+    return data[sortHeaderId];
+  };
+
+  // applyFilter(event: Event): void {
+  //   const filterValue = (event.target as HTMLInputElement).value;
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
+
+  //   if (this.dataSource.paginator) {
+  //     this.dataSource.paginator.firstPage();
+  //   }
+  // }
+
+  applyFilter(event: Event, column: string): void {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.columnFilters[column] = filterValue.trim().toLowerCase();
+
+    this.dataSource.filter = JSON.stringify(this.columnFilters);
+
+    // const overallFilter = Object.values(this.columnFilters).join("");
+    // this.dataSource.filter = overallFilter;
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  }
+
+  customFilterPredicate = (data: any, filter: string): boolean => {
+    const filterObject = JSON.parse(filter);
+    return Object.keys(filterObject).every((key) => {
+      const columnValue = this.getColumnValue(data, key);
+      return columnValue.includes(filterObject[key]);
+    });
+  };
+
+  getColumnValue(data: any, column: string): string {
+    const keys = column.split(".");
+    let value = data;
+    for (const key of keys) {
+      value = value[key];
+    }
+    return value ? value.toString().toLowerCase() : "";
   }
 }
